6164ffae5c349568f1681faf489c638e875363df
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2024-05-17 10:16:08 -0400

    Remove resources that support the Tanium CDM agent
    The Tanium CDM agent is being retired.  See #126 for more details.

--- terraform-build-user/remote_states.tf
+++ terraform-build-user/remote_states.tf
@@ -90,19 +90,6 @@ data "terraform_remote_state" "ansible_role_cdm_nessus_agent" {
   }
 }
 
-data "terraform_remote_state" "ansible_role_cdm_tanium_client" {
-  backend = "s3"
-
-  config = {
-    encrypt        = true
-    bucket         = "cisa-cool-terraform-state"
-    dynamodb_table = "terraform-state-lock"
-    profile        = "cool-terraform-backend"
-    region         = "us-east-1"
-    key            = "ansible-role-cdm-tanium-client/terraform.tfstate"
-  }
-}
-
 data "terraform_remote_state" "ansible_role_crowdstrike" {
   backend = "s3"
 
