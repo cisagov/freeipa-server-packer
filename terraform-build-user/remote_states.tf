b813cdf0111711b4192ff228064e6214afdb6508
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2025-07-06 12:42:22 -0400

    Revert "Temporarily modify build user to allow for enabling CDM support"
    This reverts commit c5159e125302afb67da9ebb4c7b52a32b12e51b8.

--- terraform-build-user/remote_states.tf
+++ terraform-build-user/remote_states.tf
@@ -36,12 +36,12 @@ data "terraform_remote_state" "images" {
 
 data "terraform_remote_state" "ansible_role_cdm_certificates" {
   # The only supported CDM environment is production.
-  count = terraform.workspace == "production" ? 1 : 1
+  count = terraform.workspace == "production" ? 1 : 0
 
   backend = "s3"
 
   config = {
-    bucket         = var.terraform_state_bucket
+    bucket         = "cisa-cool-terraform-state"
     dynamodb_table = "terraform-state-lock"
     encrypt        = true
     key            = "ansible-role-cdm-certificates/terraform.tfstate"
@@ -49,17 +49,17 @@ data "terraform_remote_state" "ansible_role_cdm_certificates" {
     region         = "us-east-1"
   }
 
-  workspace = terraform.workspace
+  workspace = "production"
 }
 
 data "terraform_remote_state" "ansible_role_cdm_nessus_agent" {
   # The only supported CDM environment is production.
-  count = terraform.workspace == "production" ? 1 : 1
+  count = terraform.workspace == "production" ? 1 : 0
 
   backend = "s3"
 
   config = {
-    bucket         = var.terraform_state_bucket
+    bucket         = "cisa-cool-terraform-state"
     dynamodb_table = "terraform-state-lock"
     encrypt        = true
     key            = "ansible-role-cdm-nessus-agent/terraform.tfstate"
@@ -67,17 +67,17 @@ data "terraform_remote_state" "ansible_role_cdm_nessus_agent" {
     region         = "us-east-1"
   }
 
-  workspace = terraform.workspace
+  workspace = "production"
 }
 
 data "terraform_remote_state" "ansible_role_crowdstrike" {
   # The only supported CDM environment is production.
-  count = terraform.workspace == "production" ? 1 : 1
+  count = terraform.workspace == "production" ? 1 : 0
 
   backend = "s3"
 
   config = {
-    bucket         = var.terraform_state_bucket
+    bucket         = "cisa-cool-terraform-state"
     dynamodb_table = "terraform-state-lock"
     encrypt        = true
     key            = "ansible-role-crowdstrike/terraform.tfstate"
@@ -85,7 +85,7 @@ data "terraform_remote_state" "ansible_role_crowdstrike" {
     region         = "us-east-1"
   }
 
-  workspace = terraform.workspace
+  workspace = "production"
 }
 
 data "terraform_remote_state" "users" {
